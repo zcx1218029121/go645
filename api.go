506ab94fbf68e9be60e6da2c2dd5a1fb59c28a2d
loafer <1218029121@qq.com>
2021-12-10 14:31:12 +0800

    :bug:

--- api.go
+++ api.go
@@ -3,7 +3,9 @@ package go645
 type Client interface {
 	ClientProvider
 	//Read 发送读请求
-	Read(address Address, itemCode int32) (*ReadData, error)
+	Read(address Address, itemCode int32) (*ReadData, bool, error)
 	//ReadWithBlock  读请求使能块
 	ReadWithBlock(address Address, data ReadRequestData) (*Protocol, error)
+	//广播信息
+	Broadcast(p *Protocol) error
 }
